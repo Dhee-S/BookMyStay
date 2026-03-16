58b8ed39d34be68403e52c429e3f8bb151038221
Dhee-S <ds8400@srmist.edu.in>
2026-03-16 09:13:05 +0530

    UC1 updated

---
+++ App/src/UC1.java
@@ -0,0 +1,8 @@
+public class UC1 {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System\n");
+        System.out.println("System initialized successfully.\n");
+
+    }
+}
